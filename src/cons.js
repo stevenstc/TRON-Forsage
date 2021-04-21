081a90d39a6cd1c43f9f41b0798d7c5156442a6e
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-04-21 15:39:26 -0500

    Update first

---
+++ src/cons.js
@@ -0,0 +1,8 @@
+const proxy = "https://proxy-ewozx.herokuapp.com/";
+const PRU = "shasta1.";// shasta1. para inhabilitar red de pruebas
+
+const WS = "TCM7j4VVHkkBH9DJs4FmX8PbWg89hjkxsM";//T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb recibe los huerfanos por defecto
+
+const SC = "TN7NHCMKT5nFV6SrLbEN27C5hYDbcRSeBT";// direccion del contrato
+
+export default {proxy, PRU, WS,  SC};
